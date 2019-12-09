c4f444c4c23d840c82d1d9c09e5bdd8a8f92074b
Dave <dave@nanoevb.com>
2019-12-09 09:16:56 -0500

    Fixed error- wrong MCS file name

--- Sample-Projects/Project-0/FPGA/tcl/prog-flash.tcl
+++ Sample-Projects/Project-0/FPGA/tcl/prog-flash.tcl
@@ -7,7 +7,7 @@ set_property PROGRAM.VERIFY  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_
 set_property PROGRAM.CHECKSUM  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a50t_0] 0]]
 refresh_hw_device [lindex [get_hw_devices xc7a50t_0] 0]
 set_property PROGRAM.ADDRESS_RANGE  {use_file} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a50t_0] 0]]
-set_property PROGRAM.FILES [list "../mcs/proj0.mcs" ] [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a50t_0] 0]]
+set_property PROGRAM.FILES [list "../mcs/EVB4.mcs" ] [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a50t_0] 0]]
 set_property PROGRAM.PRM_FILE {} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a50t_0] 0]]
 set_property PROGRAM.UNUSED_PIN_TERMINATION {pull-none} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a50t_0] 0]]
 
